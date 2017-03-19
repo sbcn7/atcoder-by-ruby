e06bacfc56ed25432166e3cd31227ec4997f9f3f
hikaru <sbyh52@gmail.com>
2017-03-20 01:59:17 +0900

    Clean abc002_B

--- abc002/B.rb
+++ abc002/B.rb
@@ -1,10 +1,3 @@
 # http://abc002.contest.atcoder.jp/tasks/abc002_2
 
-str = gets
-del_characters = %w[a i u e o]
-
-del_characters.each { |c|
-  str = str.delete(c)
-}
-
-puts(str)
+puts(gets.delete("aiueo"))
