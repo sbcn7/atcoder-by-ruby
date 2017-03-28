dd6f4713f9a8e4bd288314e02988856f87de170b
hikaru <sbyh52@gmail.com>
2017-03-29 00:04:33 +0900

    Clean abc004_B

--- abc004/B.rb
+++ abc004/B.rb
@@ -1,9 +1,6 @@
 # http://abc004.contest.atcoder.jp/tasks/abc004_2
 
 c = []
-c.push(gets.split)
-c.push(gets.split)
-c.push(gets.split)
-c.push(gets.split)
 
-puts c.reverse.map { |item| item.reverse.join(' ') }
+4.times { c.push gets.chomp }
+puts c.reverse.map(&:reverse)
