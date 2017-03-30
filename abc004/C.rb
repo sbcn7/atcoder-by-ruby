9d72e21e4d271fbb0f45e619ccb0eefc8acd1d04
hikaru <sbyh52@gmail.com>
2017-03-30 23:40:50 +0900

    Clean abc004_C

--- abc004/C.rb
+++ abc004/C.rb
@@ -1,16 +1,8 @@
 # http://abc004.contest.atcoder.jp/tasks/abc004_3
 
-cards = [*1..6]
 N = gets.to_i
-N1 = N / 5 % 6
-N2 = N % 5
 
-for i in 0...N1 do
-  cards.push cards.shift
-end
-
-for i in 0...N2 do
-  cards[i % 5], cards[i % 5 + 1] = cards[i % 5 + 1], cards[i % 5]
-end
+cards = [*1..6].rotate(N / 5 % 6)
+cards = cards[0..N % 5].rotate + cards[N % 5 + 1..5]
 
 puts cards.join
