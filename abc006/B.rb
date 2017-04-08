2e0bb1cc27038165775007f5de7d2e0a19c4a2ba
hikaru <sbyh52@gmail.com>
2017-04-08 23:37:02 +0900

    Fix abc006_B

--- abc006/B.rb
+++ abc006/B.rb
@@ -1,20 +1,11 @@
 # http://abc006.contest.atcoder.jp/tasks/abc006_2
 
 n = gets.to_i
-@memo = Array.new(n)
-
-def tori(i)
-  if i < 2
-    return 0
-  elsif i == 2
-    return 1
-  else
-    @memo[i - 3] = tori(i - 3) if @memo[i - 3].nil?
-    @memo[i - 2] = tori(i - 2) if @memo[i - 2].nil?
-    @memo[i - 1] = tori(i - 1) if @memo[i - 1].nil?
-    @memo[i] = @memo[i - 3] + @memo[i - 2] + @memo[i - 1]
-    return @memo[i]
-  end
+a = [0, 0, 1]
+ 
+n.times do |i|
+  next if i < 3
+  a.push (a[i - 1] + a[i - 2] + a[i - 3]) % 10007
 end
-
-puts tori(n - 1) % 10007
+ 
+puts a[n - 1]
