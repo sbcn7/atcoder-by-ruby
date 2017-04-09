7fd41171e15a4ca2481a25b845d52168da46154b
hikaru <sbyh52@gmail.com>
2017-04-09 18:35:19 +0900

    Clean abc006_C

--- abc006/C.rb
+++ abc006/C.rb
@@ -2,33 +2,13 @@
 
 N, M = gets.split.map(&:to_i)
 ans = '-1 -1 -1'
-break_flg = (2 * N > M || 4 * N < M) ? true : false
 
-if 2 * N / 2 + 4 * (N - N / 2) <= M
-  (0..N).each do |i|
-    break if break_flg
-    (0..N - i).each do |j|
-      sum = 2 * i + 3 * j + 4 * (N - i - j)
-      if sum == M
-        ans = "#{i} #{j} #{N - i - j}"
-      elsif sum < M
-        break_flg = true
-        break
-      end
-    end
-  end
-else
-  N.downto(0) do |i|
-    break if break_flg
-    (N - i).downto(0) do |j|
-      sum = 2 * i + 3 * j + 4 * (N - i - j)
-      if sum == M
-        ans = "#{i} #{j} #{N - i - j}"
-      elsif sum > M
-        break_flg = true
-        break
-      end
-    end
+(0..N).each do |i|
+  x = 4 * N - 2 * i - M
+  y = M + i - 3 * N
+  if x >= 0 && y >= 0
+    ans = "#{i} #{x} #{y}"
+    break
   end
 end
 
