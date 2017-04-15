4013f0587c38946b8ef65cd3968ba387667024a9
hikaru <sbyh52@gmail.com>
2017-04-15 23:45:50 +0900

    Clean abc007_B

--- abc007/B.rb
+++ abc007/B.rb
@@ -1,12 +1,5 @@
 # http://abc007.contest.atcoder.jp/tasks/abc007_2
 
 A = gets.chomp
-alps = ('a'..'z').to_a
 
-if A.size > 1
-  puts A.chop
-elsif A == 'a'
-  puts '-1'
-else
-  puts alps[alps.index(A) - 1]
-end
+puts A == 'a' ? '-1' : 'a'
