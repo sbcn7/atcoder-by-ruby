a43d939bdc77e9d9ed9e8badf6ba1bc7a02af10d
hikaru <sbyh52@gmail.com>
2017-05-10 23:50:01 +0900

    途中

--- abc009/C.rb
+++ abc009/C.rb
@@ -10,12 +10,16 @@ S.each_index do |idx|
     tmp = s.clone
     idx_min = s[idx..N - 1].index(s_min) + idx
     tmp[idx], tmp[idx_min] = s[idx_min], s[idx]
-  count = 0
-  S.each_index do |i|
-    count += 1 unless S[i] == tmp[i]
+    count = 0
+    S.each_index do |i|
+      count += 1 unless S[i] == tmp[i]
+    end
+#p "count: #{count}"
+#p s
+#p tmp
+    count <= K
   end
-    count < K
-  end
+#p "s_target: #{s_target}"
 
   if !s_target.nil? && s_target != s[idx]
     idx_target = s[idx..N - 1].index(s_target) + idx
