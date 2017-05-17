e016c9f2a2b8cd0a45363fb84786c0d79a89d79a
hikaru <sbyh52@gmail.com>
2017-05-17 23:43:01 +0900

    Fix abc009_C

--- abc009/C.rb
+++ abc009/C.rb
@@ -1,23 +1,45 @@
 # http://abc009.contest.atcoder.jp/tasks/abc009_3
 
 N, K = gets.split.map(&:to_i)
-S = gets.chomp.split('')
-ans = ''
+@S = gets.chomp.split('')
+ans = []
 
 def diff(a, b)
-  # a固定で、Sに対するa+bの最小入れ替え回数をカウントする
+  # a固定で、@Sに対するa+bの最小入れ替え回数をカウントする
+  tmp = []
+  count = 0
+  for i in 0...a.size do
+    count += 1 if a[i] != @S[i]
+  end
+
+  b.each_char do |x|
+    for idx in 0...b.size do
+      if x == @S[a.size + idx] && tmp[idx].nil?
+        tmp[idx] = x
+        break
+      elsif idx == b.size - 1
+        count += 1
+      end
+    end
+  end
+  count
 end
 
-for i in 0...S.length do
+for i in 0...@S.length do
   # t: まだ使える文字
-  S.each do |s|
-    t.push s if S.count(s) > ans.count(s)
+  t = []
+  @S.each do |s|
+    t.push s if @S.count(s) > t.count(s) + ans.count(s)
   end
 
   t.sort.each do |t_char|
-    if diff(ans + t_char, t.sort.shift) <= K
+    tmp = t.clone
+    tmp.delete_at(t.index(t_char))
+    if diff(ans.join + t_char, tmp.join) <= K
       ans.push t_char
       break
     end
   end
 end
+
+puts ans.join
