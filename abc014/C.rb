beb594ab38784089f2321b460e3b4af8cd91132c
hikaru <sbyh52@gmail.com>
2017-05-23 23:46:07 +0900

    Update abc014_C (score: 30/100)

--- abc014/C.rb
+++ abc014/C.rb
@@ -1,18 +1,15 @@
 # http://abc014.contest.atcoder.jp/tasks/abc014_3
 
 n = gets.to_i
-range = []
+ranges = []
 
-n.times do
-  range.push gets.split.map(&:to_i)
+n.times { ranges << gets.split.map(&:to_i) }
+
+max = 0
+ranges.flatten.each do |p|
+  p_count = 0
+  ranges.each { |first, last| p_count += 1 if p >= first && p <= last }
+  max = [max, p_count].max
 end
 
-count = Array.new(1_000_001, 0)
-
-range.each do |a, b|
-  (a..b).each do |i|
-    count[i] += 1
-  end
-end
-
-puts count.max
+puts max
