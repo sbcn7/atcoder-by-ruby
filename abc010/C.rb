3083ca4174d67bb295b51dfea109f72a6133928b
hikaru <sbyh52@gmail.com>
2017-05-25 23:51:41 +0900

    Fix abc010_C

--- abc010/C.rb
+++ abc010/C.rb
@@ -9,3 +9,11 @@ n.times { girls << gets.split.map(&:to_i) }
 def get_distance(x_begin, y_begin, x_end, y_end)
   Math.sqrt((x_end - x_begin)**2 + (y_end - y_begin)**2)
 end
+
+judge = girls.any? do |x, y|
+  first_dist = get_distance(tx_a, ty_a, x, y)
+  last_dist = get_distance(x, y, tx_b, ty_b)
+  first_dist + last_dist <= T * V
+end
+
+puts judge ? 'YES' : 'NO'
