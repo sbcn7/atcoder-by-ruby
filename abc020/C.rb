649f1ef568677b9e046dca1f7f34e40295355fe4
hikaru <sbyh52@gmail.com>
2017-06-24 22:45:45 +0900

    abc020_C 途中

--- abc020/C.rb
+++ abc020/C.rb
@@ -9,9 +9,27 @@ H.times do |i|
     G = [i, j] if s[i][j] == 'G'
   end
 end
-p S
-p G
 
-(T - 1).downto(1) do |x|
-  # x秒でゴール可能か調べる
+dist_y, dist_x = G - S
+path_list = Array.new(dist_y, 'y') + Array.new(dist_x, 'x')
+time = 0
+
+(T - 1).downto(1) do |x_time|
+  path_list.permutation(path_list.size) do |path|
+    time = 0
+    px = 0
+    py = 0
+    path.each do |dir|
+      px += 1 if dir == 'x'
+      py += 1 if dir == 'y'
+p px
+p py
+      time += s[py][px].equal?('#') ? x_time : 1
+p "-----"
+    end
+    break if time <= T
+  end
+  break if time <= T
 end
+
+puts time
