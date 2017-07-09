1ed6400d20295f160146298a8e62a248c32c6f46
hikaru <sbyh52@gmail.com>
2017-07-09 23:57:05 +0900

    abc025_C 途中

---
+++ abc025/C.rb
@@ -0,0 +1,13 @@
+# http://abc025.contest.atcoder.jp/tasks/abc025_c
+
+b = Array.new(2) { gets.split.map(&:to_i) }
+c = Array.new(3) { gets.split.map(&:to_i) }
+pattern = Array.new('o', 5) + Array.new('x', 4)
+point_o = 0
+point_x = 0
+
+pattern.permutation do |ox|
+  (0..2).each do |i_b|
+    
+  end
+end
