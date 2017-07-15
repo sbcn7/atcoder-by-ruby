77c3ed65261c5f1ab1330c9a37f64f991836b633
sbcn7 <sbyh52@gmail.com>
2017-07-15 23:49:25 +0900

    abc027_B 途中

--- abc027/B.rb
+++ abc027/B.rb
@@ -2,5 +2,13 @@
 
 N = gets.to_i
 a = gets.split.map(&:to_i)
+count = 0
 
-
+if a.inject { |sum, n| sum + n } % N == 0
+  a.each do |i|
+  	# a[i]の島の左右の人数で橋を架けるか判定する
+    count += 1
+  end
+else
+  puts -1 
+end
