2d91c4d91d09f93377a81272251e7e2a5db05eb8
hikaru <sbyh52@gmail.com>
2017-08-05 22:09:55 +0900

    Fix abc033_B

--- abc033/B.rb
+++ abc033/B.rb
@@ -2,11 +2,8 @@
 
 N = gets.to_i
 S_P = Array.new(N) { gets.chomp.split }
+ans_idx = nil
+population = S_P.transpose[1].map(&:to_i).inject(:+)
 
-population = S_P.transpose[1].map(&:+)
-
-S_P.each_index do |i|
-  ans_idx = i if S_P[i][1].to_i * 2 >= population
-end
-
+S_P.each_index { |i| ans_idx = i if S_P[i][1].to_i * 2 > population }
 puts ans_idx.nil? ? 'atcoder' : S_P[ans_idx][0]
