be074fc0c1dec07b44318a06c8b9b704e4379778
hikaru <sbyh52@gmail.com>
2017-09-12 23:50:24 +0900

    Add abc045_B

--- abc045/B.rb
+++ abc045/B.rb
@@ -1,7 +1,22 @@
 # http://abc045.contest.atcoder.jp/tasks/abc045_b
 
-sa = gets.chomp
-sb = gets.chomp
-sc = gets.chomp
+sa = gets.chomp.split('')
+sb = gets.chomp.split('')
+sc = gets.chomp.split('')
+target = 'a'
 
+while !target.nil? do
+  case target
+  when 'a' then
+    ans = 'A' if sa.length.zero?
+    target = sa.shift
+  when 'b' then
+    ans = 'B' if sb.length.zero?
+    target = sb.shift
+  when 'c' then
+    ans = 'C' if sc.length.zero?
+    target = sc.shift
+  end
+end
 
+puts ans
